c19239dcb8abe4b78f9c8ca5f21db573593bbb6f
Dave Jacobowitz <david.jacobowitz@gmail.com>
2022-04-21 19:50:30 -0700

    missing fie

---
+++ stlib/commands/tstamp.py
@@ -0,0 +1,39 @@
+import sys, datetime, time, re
+
+
+from .. import util
+
+class TSDecoder:
+    def __init__(self, config):
+        self.config = config
+
+
+    def usage(self, adict):
+        scname = adict.get('subcommand')
+        usage = []
+        usage.append(scname)
+        return usage
+
+
+    def run(self, adict):
+        scname = adict.get('subcommand')
+  
+        if len(adict.get('args',[])) < 1:
+            now = time.time()
+            adict['args'].append(now)
+            print(f'Returning the CURRENT time: {now}')
+
+        try:
+            ts = float(adict['args'][0])
+        except Exception as e:
+            print(f'-Error- {repr(e)}')
+            return   
+
+        t = datetime.datetime.fromtimestamp(ts, tz=datetime.timezone.utc)
+        ts = re.sub(r'\+[\d:]+$','',t.isoformat())
+        print(f'{t.tzinfo.tzname(None)} Time: {ts}')
+    
+        ltz = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
+        lt  = t + ltz.utcoffset(None)
+        lts = re.sub(r'\+[\d:]+$','',lt.isoformat())
+        print(f'{ltz.tzname(None)} Time: {lts}')
