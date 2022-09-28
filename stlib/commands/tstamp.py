64d93cda676843de491d96d388aa77a720ba3970
Jeremy Elson <jelson@gmail.com>
2022-09-27 18:47:19 -0700

    Improve timestamp command

--- stlib/commands/tstamp.py
+++ stlib/commands/tstamp.py
@@ -1,5 +1,10 @@
 import sys, datetime, time, re
 
+try:
+    import dateparser
+    has_dateparser = True
+except:
+    has_dateparser = False
 
 from .. import util
 
@@ -7,34 +12,40 @@ class TSDecoder:
     def __init__(self, config):
         self.config = config
 
-
     def usage(self, adict):
         scname = adict.get('subcommand')
         usage = []
         usage.append(scname)
         return usage
 
-
     def run(self, adict):
         scname = adict.get('subcommand')
-  
-        if len(adict.get('args',[])) < 1:
-            now = time.time()
-            adict['args'].append(now)
-            print(f'Returning the CURRENT time: {now}')
+        args = adict.get('args', [])
 
-        try:
+        prefix = ""
+        utc_dt = None
+
+        if len(args) < 1:
+            prefix = "CURRENT "
+            utc_dt = datetime.datetime.now().astimezone(datetime.timezone.utc)
+        elif has_dateparser:
+            localzone = datetime.datetime.now().astimezone().tzinfo
+            utc_dt = dateparser.parse(" ".join(args), settings={
+                'TIMEZONE': str(localzone),
+                'TO_TIMEZONE': 'UTC',
+                'RETURN_AS_TIMEZONE_AWARE': True,
+            })
+
+        # parse as float if we don't have dateparser, or if the dateparser fails
+        if not utc_dt:
             ts_str = re.search(r'([\.0-9]+)', adict['args'][0].strip())[1]
             ts = float(ts_str)
-        except Exception as e:
-            print(f'-Error- {repr(e)}')
-            return   
+            utc_dt = datetime.datetime.fromtimestamp(ts, tz=datetime.timezone.utc)
 
-        t = datetime.datetime.fromtimestamp(ts, tz=datetime.timezone.utc)
-        ts = re.sub(r'\+[\d:]+$','',t.isoformat())
-        print(f'{t.tzinfo.tzname(None)} Time: {ts}')
-    
-        ltz = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
-        lt  = t + ltz.utcoffset(None)
-        lts = re.sub(r'\+[\d:]+$','',lt.isoformat())
-        print(f'{ltz.tzname(None)} Time: {lts}')
+
+        def _show(prefix, dt):
+            print(f'{prefix}{dt.tzinfo.tzname(None)}  Time {dt.isoformat()[:-6]}')
+
+        print(f'{prefix}UNIX Time {utc_dt.timestamp()}')
+        _show(prefix, utc_dt)
+        _show(prefix, utc_dt.astimezone())
